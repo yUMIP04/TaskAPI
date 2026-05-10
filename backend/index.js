ef8f04741f00d73851f4f4f77447838f15e488a4
yUMIP04 <yumip5719@gmail.com>
2026-05-10 14:56:57 -0600

    mysql2 se instalo y esta la ruta de registro

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
+app.use(express.json())
+
+/*🌟 RUTA DEL SERVIDOR */
 app.get("/", (req, res) => {
 
     res.send("Servidor funcionando");
@@ -9,6 +14,30 @@ app.get("/", (req, res) => {
 });
 
 
+
+
+/*🌟 RUTA DEL SERVIDOR */
+
+app.post("/registro", (req, res) =>{
+
+    const correo_user = req.body.correo;
+    const pass_user = req.body.password;
+
+    if(correo_user && pass_user){
+
+        res.json({
+            correo : correo_user,
+            contraseña : pass_user 
+        });
+    } else{
+
+        res.status(400).json({
+            error : "Correo y contraseña no requeridos o no encotrados"
+        });
+    }
+});
+
+/*🌟 Escuchar servidor */
 app.listen(3000, () => {
 
     console.log("Servidor en http://localhost:3000");
