28953ad3696de2752f6ce8b933e3f586bedbb2b4
yUMIP04 <yumip5719@gmail.com>
2026-05-12 19:49:11 -0600

    Creacion de la tabla Usuario

--- backend/db.js
+++ backend/db.js
@@ -13,9 +13,19 @@ connection = await mysql.createConnection({
 });
 
 console.log("🥳 Se logro la conexion a la BD");
+ const [prueba] = await connection.query('SELECT  "Hola desde MYSQL" AS mensaje');
+    console.log("Resultado del SELECT: " + prueba[0].mensaje);
+
+    const tabla_usuarios =await connection.query(`
+        CREATE TABLE IF NOT EXISTS Usuarios(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+         correo VARCHAR(255) NOT NULL, 
+         password VARCHAR(255) NOT NULL)`);
+
+         console.log("📋 La tabla Usuarios se creo correctamente");
 }catch(e){
 
-    console.error(`Hubo un erro al conectarse a la BD: ${e}`);
+    console.error(`Hubo un error al conectarse a la BD: ${e}`);
 }
 
 export default connection;
