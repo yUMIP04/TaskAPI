ebb72cc81d507f14b7a1e60d6ff36a0dd04e0e9d
yUMIP04 <yumip5719@gmail.com>
2026-05-15 23:16:26 -0600

    El login funcionando

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,43 @@ app.post("/registro", async (req, res) =>{
     }
 });
 
+    /*RUTA PARA LOGIN */
+
+    app.post("/login", async (req,res) =>{
+
+        const correo_usuario = req.body.correo;
+        const pass_usuario = req.body.password;
+
+        const sql_consulta = 'SELECT password FROM Usuarios WHERE correo = ? LIMIT 1';
+
+        const [consulta_correo] = await connection.execute(sql_consulta, [correo_usuario]);
+
+        if(consulta_correo.length > 0){
+
+            res.status(200);
+
+            /*validacion de contraseña */
+
+            const coinciden = await bcrypt.compare(pass_usuario, consulta_correo[0].password);
+            if(coinciden){
+
+                res.json({
+                    mensaje: "La contraseña coincide"
+                }).status(200);
+            } else{
+
+                res.json({
+                    mensaje: "La contraseña no coincide"
+                }).status(401)
+            }
+        } else{
+
+            res.json({
+                error: `No se encontro ninguna coincidencia`
+            }).status(401)
+        }
+    })
+
 /*🌟 Escuchar servidor */
 app.listen(3000, () => {
 
