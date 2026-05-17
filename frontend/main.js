8379ae06e79769e4d3c5a0769f167ca7f6778223
yUMIP04 <yumip5719@gmail.com>
2026-05-16 19:40:17 -0600

    Comenzando la parte del frontend

---
+++ frontend/main.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById("formulario-Login");
+
+formulario.addEventListener("submit", (event) =>{
+
+    event.preventDefault();
+
+    console.log("Formulario capturado con exito");
+})
